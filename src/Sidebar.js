634414399eedc6e0cbb8856a5778db190c7a1b44
Jonathan Won <jabinx92@gmail.com>
2021-03-25 15:16:14 -0700

    search function works on first event.target.value

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,12 @@
-import { Avatar, IconButton } from '@material-ui/core'
-import React, { useEffect, useState } from 'react'
-import "./Sidebar.css"
-import SearchIcon from '@material-ui/icons/Search'
-import { RateReviewOutlined } from '@material-ui/icons'
-import { SidebarChat } from './SidebarChat'
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
-import db, { auth } from "./firebase"
+import { Avatar, IconButton } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import './Sidebar.css';
+import SearchIcon from '@material-ui/icons/Search';
+import { RateReviewOutlined } from '@material-ui/icons';
+import { SidebarChat } from './SidebarChat';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import db, { auth } from './firebase';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
@@ -28,86 +28,99 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function Sidebar(props) {
-    const user = useSelector(selectUser);
-    const [chats, setChats] = useState([]);
-    const classes = useStyles();
-    const [open, setOpen] = useState(false);
-    const [search, setSearch] = useState('')
+  const user = useSelector(selectUser);
+  const [chats, setChats] = useState([]);
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState('');
 
-    const handleOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-    useEffect(() => {
-      db.collection('chats').onSnapshot(snapshot => (
-        setChats(snapshot.docs.map( doc => ({
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    db.collection('chats').onSnapshot((snapshot) =>
+      setChats(
+        snapshot.docs.map((doc) => ({
           id: doc.id,
           data: doc.data(),
-        })))
-      ))
-    }, [])
+        }))
+      )
+    );
+  }, []);
 
-    const addChat = () => {
-      const chatName = prompt('Please enter a chat name');
-      if(chatName){
-        db.collection('chats').add({
-          chatName: chatName
-        })
-      }
+  const addChat = () => {
+    const chatName = prompt('Please enter a chat name');
+    if (chatName) {
+      db.collection('chats').add({
+        chatName: chatName,
+      });
     }
+  };
 
-    const searchFunction = (e) => {
-      setSearch(e.target.value)
-      console.log(search)
-      console.log(chats)
-      const filtered = chats.filter(chat => {
-        return chat.data.chatName.toLowerCase().includes(search.toLowerCase())
-      })
-      // console.log(filtered)
-      setChats(filtered)
-    }
-    return (
+  const searchFunction = (e) => {
+    setSearch(e)
+    console.log(search)
+    console.log(chats)
+    const filtered = chats.filter(chat => {
+      return chat.data.chatName.toLowerCase().includes(e.toLowerCase())
+    })
+    // console.log(filtered)
+    setChats(filtered)
+  };
+  
+  return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar onClick={handleOpen} src={user.photo} className='sidebar__avatar'/>
+        <Avatar
+          onClick={handleOpen}
+          src={user.photo}
+          className="sidebar__avatar"
+        />
         <div className="sidebar__input">
           <SearchIcon />
-          <input placeholder="Search" value={search} onChange={e => searchFunction(e)}/>
+          <input
+            placeholder="Search"
+            value={search}
+            onChange={(e) => searchFunction(e.target.value)}
+          />
         </div>
-          <IconButton variant='outlined' className='sidebar__inputButton'>
-            <RateReviewOutlined onClick={addChat}/>
-          </IconButton>
+        <IconButton variant="outlined" className="sidebar__inputButton">
+          <RateReviewOutlined onClick={addChat} />
+        </IconButton>
       </div>
       <div className="sidebar__chats">
-          {chats.map(({id, data: {chatName}}) => (
-            <SidebarChat key={id} id={id} chatName={chatName}/>
-          ))}
+        {chats.map(({ id, data: { chatName } }) => (
+          <SidebarChat key={id} id={id} chatName={chatName} />
+        ))}
       </div>
 
       <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className={classes.modal}
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-            <div className={classes.paper}>
-              <h2 id="transition-modal-title">Are you sure you want to sign out?</h2>
-              <button onClick={() => auth.signOut()}>Yes</button>
-              <button onClick={handleClose}>No</button>
-            </div>
-          </Fade>
-        </Modal>
-      </div>
-    )
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">
+              Are you sure you want to sign out?
+            </h2>
+            <button onClick={() => auth.signOut()}>Yes</button>
+            <button onClick={handleClose}>No</button>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
