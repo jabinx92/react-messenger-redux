8a812908a1792c64f92d069347417b4ad73e0b07
Jonathan Won <jabinx92@gmail.com>
2021-03-24 19:35:07 -0700

    implement signout feature

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,11 +7,39 @@ import { SidebarChat } from './SidebarChat'
 import { useSelector } from 'react-redux'
 import { selectUser } from './features/userSlice'
 import db, { auth } from "./firebase"
-import Signout from './Signout'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export function Sidebar(props) {
     const user = useSelector(selectUser);
-    const [chats, setChats] = useState([])
+    const [chats, setChats] = useState([]);
+    const classes = useStyles();
+    const [open, setOpen] = useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
 
     useEffect(() => {
       db.collection('chats').onSnapshot(snapshot => (
@@ -32,24 +60,46 @@ export function Sidebar(props) {
     }
 
     return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                {/* <Avatar onClick={() => auth.signOut()} src={user.photo} className='sidebar__avatar'/> */}
-                <Avatar onClick={() => alert(<Signout/>)} src={user.photo} className='sidebar__avatar'/>
-                <div className="sidebar__input">
-                    <SearchIcon />
-                    <input placeholder="Search"/>
-                </div>
-                  <IconButton variant='outlined' className='sidebar__inputButton'>
-                    <RateReviewOutlined onClick={addChat}/>
-                  </IconButton>
-            </div>
-
-            <div className="sidebar__chats">
-                {chats.map(({id, data: {chatName}}) => (
-                  <SidebarChat key={id} id={id} chatName={chatName}/>
-                ))}
-            </div>
+    <div className="sidebar">
+      <div className="sidebar__header">
+        {/* <Avatar onClick={() => auth.signOut()} src={user.photo} className='sidebar__avatar'/> */}
+        <Avatar onClick={handleOpen} src={user.photo} className='sidebar__avatar'/>
+        <div className="sidebar__input">
+          <SearchIcon />
+          <input placeholder="Search"/>
         </div>
+          <IconButton variant='outlined' className='sidebar__inputButton'>
+            <RateReviewOutlined onClick={addChat}/>
+          </IconButton>
+      </div>
+
+      <div className="sidebar__chats">
+          {chats.map(({id, data: {chatName}}) => (
+            <SidebarChat key={id} id={id} chatName={chatName}/>
+          ))}
+      </div>
+
+      <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Are you sure you want to sign out?</h2>
+              <p id="transition-modal-description" onClick={() => auth.signOut()}>Yes</p>
+              <p id="transition-modal-description" onClick={handleClose}>No</p>
+
+            </div>
+          </Fade>
+        </Modal>
+      </div>
     )
 }
