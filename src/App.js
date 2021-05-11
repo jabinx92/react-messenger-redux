30917d4e5d97cf084cdef06727d5cde9609694ff
Jonathan Won <jabinx92@gmail.com>
2021-05-11 15:34:26 -0700

    format app page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { selectUser, login, logout } from './features/userSlice';
 import Messenger from './Messenger';
-import  Login  from './Login';
-import {auth} from "./firebase";
+import Login from './Login';
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selectUser);
@@ -12,15 +12,15 @@ function App() {
 
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      if(authUser) {
+      if (authUser) {
         //user is logged in
         dispatch(
           login({
-          uid: authUser.uid,
-          photo: authUser.photoURL,
-          email: authUser.email,
-          displayName: authUser.displayName,
-        })
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
         );
       } else {
         //user is logged out
