1da4c2c71f758c645ed10ed97dd8be75c8db9f90
Jonathan Won <jabinx92@gmail.com>
2021-05-14 13:34:35 -0700

    format messaging page

--- src/Message.js
+++ src/Message.js
@@ -4,20 +4,20 @@ import { useSelector } from 'react-redux'
 import { selectUser } from './features/userSlice'
 import './Message.css'
 
-const Message = forwardRef(({id, contents: {
-    timestamp, displayName, email, message, photo, uid} }, ref) => {
-    const user = useSelector(selectUser)
-  
-    return (
-      <>
-        <div 
+const Message = forwardRef(({ id, contents: {
+  timestamp, displayName, email, message, photo, uid } }, ref) => {
+  const user = useSelector(selectUser)
+
+  return (
+    <>
+      <div
         ref={ref}
         className={`message ${user.email === email && 'message__sender'}`}>
-          <Avatar className='message__photo' src={photo}/>
-          <p>{message}</p>
-          <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
-        </div>  
-      </>
-    )
+        <Avatar className='message__photo' src={photo} />
+        <p>{message}</p>
+        <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
+      </div>
+    </>
+  )
 })
 export default Message
