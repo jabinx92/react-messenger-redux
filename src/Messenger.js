66c496b2ac15e4a92d0d449b6d7877841c1031b1
Jonathan Won <jabinx92@gmail.com>
2021-05-17 18:48:07 -0700

    format messenger.js page

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Chat } from './Chat';
 import './Messenger.css';
-import {Sidebar} from './Sidebar'
+import { Sidebar } from './Sidebar'
 
 function Messenger() {
-    
+
 
     return (
         <div className="messenger">
